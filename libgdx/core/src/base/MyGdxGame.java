771b72e41ed9832665414fe502c1a9dfca231b4d
Eugene Wang <eaglgenes101@gmail.com>
2015-10-11 21:44:11 -0400

    Added license notices to files

--- libgdx/core/src/base/MyGdxGame.java
+++ libgdx/core/src/base/MyGdxGame.java
@@ -19,6 +19,25 @@ import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
+/*
+The LibGDX "main" method
+Copyright (C) 2015 Eugene "eaglgenes101" Wang
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 public class MyGdxGame implements ApplicationListener
 {
 
